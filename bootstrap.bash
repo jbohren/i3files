1e7d9c6d41f51e4baf200d785ed9885697528f76
Jonathan Bohren <jbo@jhu.edu>
2013-09-17 16:18:59 -0400

    adding bootstrap script to install i3

---
+++ bootstrap.bash
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+INFO="[i3 bootstrap]"
+i3_VERSION_MIN="4.6"
+
+# Bootstrap an i3 install
+i3_FOUND="$(command -v i3)"
+
+verlte() {
+  [ "$1" != "$(echo -e '$1\n$2' | sort -Vr | head -n1)" ]
+}
+
+verlt() {
+  [ "$1" = "$2" ] && return 1 || verlte $1 $2
+}
+
+# Check if i3 is installed
+if [ -n "$i3_FOUND" ] ; then 
+  # Check what version is installed
+  i3_VERSION="$(i3 --version | sed 's/^.*\([0-9]\.[0-9]\).*$/\1/g')"
+fi
+
+if verlt "$i3_VERSION" "$i3_VERSION_MIN" ; then
+  echo "$INFO Found i3 version $i3_VERSION < $i3_VERSION_MIN"
+fi
+
+if [ -z "$i3_FOUND" ] || verlt "$i3_VERSION" "$i3_VERSION_MIN" ; then 
+  echo "$INFO Installing i3 version $i3_VERSION_MIN ..."
+  sudo su
+  echo "deb http://debian.sur5r.net/i3/ $(lsb_release -c -s) universe" >> /etc/apt/sources.list
+  apt-get update
+  apt-get --allow-unauthenticated install sur5r-keyring
+  apt-get update
+  apt-get install i3
+fi
+
+i3_VERSION="$(i3 --version | sed 's/^.*\([0-9]\.[0-9]\).*$/\1/g')"
+echo "$INFO i3 version $i3_VERSION is installed"
--- i3.bash
+++ i3.bash
@@ -6,14 +6,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 # Generate the full config based on the hostname
 cat $DIR/config.base $DIR/config.$HOSTNAME > $DIR/config
 
-
 # Check if i3 is already running
-if ps aux | grep "i3" > /dev/null
-then
+if ps aux | grep "i3" > /dev/null; then
+  # Just restart it to use the new config
   i3 restart
 else
-  if ps aux | grep "xfwm4" > /dev/null
-  then
-    killall xfwm4 && i3 &
+  # Replace other _lesser_ WMs
+  if ps aux | grep "xfwm4" > /dev/null; then
+    killall xfwm4
   fi
+  # Start i3
+  i3 &
 fi
