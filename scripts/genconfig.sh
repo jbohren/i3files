b33d3afff35a5556f0a7e1f5762398f8421bc490
Jonathan Bohren <jonathan.bohren@gmail.com>
2014-02-25 13:33:29 -0500

    robust to missing special configs

--- scripts/genconfig.sh
+++ scripts/genconfig.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
 # Generate the full config based on the hostname
-cat $I3DIR/base.cfg $I3DIR/configs/$HOST.cfg > $I3DIR/config
+if [ -e $I3DIR/configs/$HOST.cfg ]; then
+  cat $I3DIR/base.cfg $I3DIR/configs/$HOST.cfg > $I3DIR/config
+else
+  cp $I3DIR/base.cfg > $I3DIR/config
+fi
 
