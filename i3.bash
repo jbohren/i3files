e9fa5d915547937abfd0396ca0e0dcf8910e8855
Jonathan Bohren <jbo@jhu.edu>
2014-11-22 15:00:31 -0500

    fixing bash wrapper

--- i3.bash
+++ i3.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Get the directory to this file
-I3DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+export I3DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
 source $I3DIR/scripts/genconfig.sh
 source $I3DIR/scripts/run_or_reload.sh
